a8255b5b1beb741688508ccb68f6dbed76931bc7
Mark Murphy <mmurphy@xps15.(none)>
2011-11-21 15:02:30 -0500

    switch to consistently use two spaces, not tabs

--- jni/anddown.c
+++ jni/anddown.c
@@ -1,16 +1,16 @@
 /***
-	Copyright (c) 2010 CommonsWare, LLC
-	Portions (c) somebody else who didn't bother to indicate who they were
-	
-	Licensed under the Apache License, Version 2.0 (the "License"); you may
-	not use this file except in compliance with the License. You may obtain
-	a copy of the License at
-		http://www.apache.org/licenses/LICENSE-2.0
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
+  Copyright (c) 2010 CommonsWare, LLC
+  Portions (c) somebody else who didn't bother to indicate who they were
+  
+  Licensed under the Apache License, Version 2.0 (the "License"); you may
+  not use this file except in compliance with the License. You may obtain
+  a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 */
 
 #include "com_commonsware_cwac_anddown_AndDown.h"
@@ -23,34 +23,34 @@
 
 JNIEXPORT jstring JNICALL Java_com_commonsware_cwac_anddown_AndDown_markdownToHtml
   (JNIEnv *env, jobject o, jstring raw) {
-	struct buf *ib, *ob;
-	int ret;
+  struct buf *ib, *ob;
+  int ret;
   jstring result;
 
-	struct sd_callbacks callbacks;
-	struct html_renderopt options;
-	struct sd_markdown *markdown;
+  struct sd_callbacks callbacks;
+  struct html_renderopt options;
+  struct sd_markdown *markdown;
 
   const char* str;
   str = (*env)->GetStringUTFChars(env, raw, NULL);
 
-	ib = bufnew(INPUT_UNIT);
+  ib = bufnew(INPUT_UNIT);
   bufputs(ib, str);
-	ob = bufnew(OUTPUT_UNIT);
+  ob = bufnew(OUTPUT_UNIT);
 
   (*env)->ReleaseStringUTFChars(env, raw, str);
 
-	sdhtml_renderer(&callbacks, &options, 0);
-	markdown = sd_markdown_new(0, 16, &callbacks, &options);
+  sdhtml_renderer(&callbacks, &options, 0);
+  markdown = sd_markdown_new(0, 16, &callbacks, &options);
 
-	sd_markdown_render(ob, ib->data, ib->size, markdown);
-	sd_markdown_free(markdown);
+  sd_markdown_render(ob, ib->data, ib->size, markdown);
+  sd_markdown_free(markdown);
 
   result=(*env)->NewStringUTF(env, bufcstr(ob));
 
-	/* cleanup */
-	bufrelease(ib);
-	bufrelease(ob);
+  /* cleanup */
+  bufrelease(ib);
+  bufrelease(ob);
 
   return(result);
 }
